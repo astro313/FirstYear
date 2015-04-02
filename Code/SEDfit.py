932fef6117671b45667b8b09bbc6cbd3aae006a0
Daisy Leung <leungtszkuk@gmail.com>
2015-04-01 22:35:11 -0400

    fix some pep8 error

--- Code/SEDfit.py
+++ Code/SEDfit.py
@@ -2,14 +2,14 @@ from scipy.optimize import curve_fit
 import scipy.optimize as opt
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import k, h, c 
+from scipy.constants import k, h, c
 import matplotlib
 import astropy.io.ascii
 
-matplotlib.rc('xtick', labelsize=18) 
-matplotlib.rc('ytick', labelsize=18) 
-font = {'family' : 'normal', 
-        'weight' : 'bold', 
+matplotlib.rc('xtick', labelsize=18)
+matplotlib.rc('ytick', labelsize=18)
+font = {'family' : 'normal',
+        'weight' : 'bold',
         'size'   : 12}
 matplotlib.rc('font', **font)
 
@@ -45,7 +45,7 @@ def data_readin_unit_conversion(micron, flux_mJy, err_mJy):
     return freq_Hz, flux_SI, error_SI
 
 SMG_data = read_data_fromtxt(SMGfilename)
-SMGwavelg_micron, SMG_mJy, SMG_error_mJy = ([dat[0] for dat in SMG_data], 
+SMGwavelg_micron, SMG_mJy, SMG_error_mJy = ([dat[0] for dat in SMG_data],
                                             [dat[1] for dat in SMG_data],
                                             [dat[2] for dat in SMG_data])
 
@@ -75,11 +75,11 @@ Point_Cont_Hz = np.array([1.042e11, 1.042e11, 1.02e11])     # Hz
 Point_error_Jy = np.array([5.0647e-4, 5.0647e-4, 5.0647e-4])    # Jy
 # =======================================================
 # compare to similiar FR II galaxies
-# 3C6.1: 
+# 3C6.1:
 # 7.3 mJy @ 8GHz and 10.14mJy at 1.4 GHz
 Core_3c6dot1_Jy = np.array([7.3e-3, 10.14e-3])      # Jy
 Core_3c6dot1_freq = np.array([8e9, 1.4e9])  # Hz
-# 3C 263: 
+# 3C 263:
 # 161.1 mJy @ 5 GHz and 0.119 Jy @ 8GHz
 Core_3c263_Jy = np.array([161.1e-3, 0.119])
 Core_3c263_freq = np.array([5e9, 8e9])      # Hz
@@ -107,12 +107,12 @@ def powerlaw(freq, dummyC, alpha):
 
 def lobeSyn(freq, dummyA, beta, freq_t, freq_lobe):
     """
-    Ref: 
+    Ref:
     -------
     equation 1 in CLEARY 2007
 
 
-    Input: 
+    Input:
     --------
     freq: Hz
     dummyA is scaling factor
@@ -143,7 +143,7 @@ def chi2func(theta, freq, flux, err):
 
     """
 
-    model = lobeSyn(freq, *theta)       
+    model = lobeSyn(freq, *theta)
     chisq = np.sum(((flux - model)/err)**2)
     print "chisq: {:.2f} ".format(chisq)
     return chisq
@@ -157,7 +157,7 @@ def B_nu(freq, freq_0, C, beta=1.5):
     boltz = k * T
     b_nu = 2.*h/c**2. * freq**3./(np.exp(h*freq/boltz)-1)
     tau = (freq/freq_0)**(beta)
-    return (1-np.exp(-tau)) * b_nu * C 
+    return (1-np.exp(-tau)) * b_nu * C
 
 def b_nuAndpower(freq, dummyB, dummyP):
     """
@@ -167,7 +167,7 @@ def b_nuAndpower(freq, dummyB, dummyP):
     ---------
     freq: Hz
 
-    Parameters: 
+    Parameters:
     ------------
     dummyB
     dummyP
@@ -201,7 +201,7 @@ def chi2CompositeFunc(theta, freq, flux, err):
 
     """
 
-    model = b_nuAndpower(freq, *theta)      
+    model = b_nuAndpower(freq, *theta)
     chisq = np.sum(((flux - model)/err)**2)
     print"chisq: {:.2f}".format(chisq)
     return chisq
@@ -212,102 +212,103 @@ SI2Jy = 1.e26
 
 
 theta_guess = [4321., 0.19, 3.e6, 3.e12]    # dummy, (CLEARY 2007 0.19, 3.e6, 3.e12)
-theta_best = opt.fmin(chi2func, theta_guess, 
-    maxiter=1.e6, maxfun=1.e6, 
+theta_best = opt.fmin(chi2func, theta_guess,
+    maxiter=1.e6, maxfun=1.e6,
     args=(Radio_Hz[7:], Radio_Jy[7:]*Jy2mJy, Radio_error[7:]*Jy2mJy))
 print"scaling = {:.2f}, beta = {:.2f}, freq_t = {:.4f} Hz, freq_lobe = {:.2f} Hz".format(*theta_best)
 
 # plot data and fit and error:
-plt.errorbar(Radio_Hz*Hz2GHz, 
-    Radio_Jy*Jy2mJy, 
-    Radio_error*Jy2mJy, 
+plt.errorbar(Radio_Hz*Hz2GHz,
+    Radio_Jy*Jy2mJy,
+    Radio_error*Jy2mJy,
     fmt='.k', ecolor='lightgray', label='NED')      # mJy
-plt.errorbar(Point_Cont_Hz*Hz2GHz, 
-    Point_Continuum_Jy*Jy2mJy, Point_error_Jy*Jy2mJy, 
+plt.errorbar(Point_Cont_Hz*Hz2GHz,
+    Point_Continuum_Jy*Jy2mJy, Point_error_Jy*Jy2mJy,
     fmt='.r', ecolor='darkgrey', label='Our continuum', ms=10 )
 
 yfit = lobeSyn(Radio_Hz[7:], *theta_best)
-plt.plot(Radio_Hz[7:]*Hz2GHz, yfit, 
+plt.plot(Radio_Hz[7:]*Hz2GHz, yfit,
     label='Lobe fit to NED data')
 
 x_syn_extend = np.linspace(10.e9, 104.21e9)
 extrapolate_mJy = lobeSyn(x_syn_extend, *theta_best)    # extrapolate the fit to 104 GHz
-plt.plot(x_syn_extend*Hz2GHz, extrapolate_mJy, ':', 
+plt.plot(x_syn_extend*Hz2GHz, extrapolate_mJy, ':',
     label='Extrapolate to 104.21GHz')
 
 core_fit_3c220dot3, core_fit_3c220dot3_covar = \
-    curve_fit(powerlaw, Core_3c220dot3_Hz, Core_3c220dot3_Jy, 
+    curve_fit(powerlaw, Core_3c220dot3_Hz, Core_3c220dot3_Jy,
         [7.728e-29, 2.5135477602042937])
 # extrapolate to 104.21Ghz using average spectral index
 extra104GHz3c220dot3_Jy = powerlaw(104.21e9, 1., -0.415)
 
-core_fit_3c6dot1, core_fit_3c6dot1_covar = curve_fit(powerlaw, Core_3c6dot1_freq, Core_3c6dot1_Jy, 
+core_fit_3c6dot1, core_fit_3c6dot1_covar = curve_fit(powerlaw, Core_3c6dot1_freq, Core_3c6dot1_Jy,
     [0.53746,-0.18853])
 
-core_fit_3c263, core_fit_3c263_covar = curve_fit(powerlaw, 
-    Core_3c263_freq, Core_3c263_Jy, 
+core_fit_3c263, core_fit_3c263_covar = curve_fit(powerlaw,
+    Core_3c263_freq, Core_3c263_Jy,
     [283943., -0.6444])
 
 print"Spectral index of 3C220.3: {:.2f}, 3C6.1: {:.2f}, 3C263: {:.2f}"\
     .format(core_fit_3c220dot3[1], core_fit_3c6dot1[1], core_fit_3c263[1])
-print 
+print
 
-plt.plot(Core_3c220dot3_Hz[0]*Hz2GHz, 
-    Core_3c220dot3_Jy[0]*Jy2mJy, 
-    'or', 
+plt.plot(Core_3c220dot3_Hz[0]*Hz2GHz,
+    Core_3c220dot3_Jy[0]*Jy2mJy,
+    'or',
     label='3c220.3 core data')
 
-plt.errorbar(Core_3c220dot3_Hz[1]*Hz2GHz, 
-    Core_3c220dot3_Jy[1]*Jy2mJy, 
-    Core_3c220dot3_Jy[1]*Jy2mJy, 
-    uplims=True, 
+plt.errorbar(Core_3c220dot3_Hz[1]*Hz2GHz,
+    Core_3c220dot3_Jy[1]*Jy2mJy,
+    Core_3c220dot3_Jy[1]*Jy2mJy,
+    uplims=True,
     label='upper limit no core')
 
-plt.plot(104.213e9*Hz2GHz, 
-    extra104GHz3c220dot3_Jy*Jy2mJy, 
-    'k+', 
-    markersize=10, 
+plt.plot(104.213e9*Hz2GHz,
+    extra104GHz3c220dot3_Jy*Jy2mJy,
+    'k+',
+    markersize=10,
     label='3c220.3 Extrapolated Core from 9GHz @ 104GHz')   # using alpha = -0.451 (average of 3c6.1 and 3c263; assuming unity scaling factor)
-plt.plot(Core_3c263_freq*Hz2GHz, 
-    Core_3c263_Jy*Jy2mJy, 
-    'sc', 
+plt.plot(Core_3c263_freq*Hz2GHz,
+    Core_3c263_Jy*Jy2mJy,
+    'sc',
     label='3c263 core data')
-plt.plot(Core_3c6dot1_freq*Hz2GHz, 
-    Core_3c6dot1_Jy*Jy2mJy, 
-    'bx', 
+plt.plot(Core_3c6dot1_freq*Hz2GHz,
+    Core_3c6dot1_Jy*Jy2mJy,
+    'bx',
     label='3c6.1 core data points')
 
 
 ###########
-# SMG 
+# SMG
 ###########
 ComposTheta_guess = [4.6264e-7, 1.e-9]
-ComposTheta_best = opt.fmin(chi2CompositeFunc, ComposTheta_guess, 
-    args=(freq_Hz[:], SMGflux_SI[:], SMGerr_SI[:])) # MBB and power
+ComposTheta_best = opt.fmin(chi2CompositeFunc,
+                            ComposTheta_guess,
+                            args=(freq_Hz[:], SMGflux_SI[:], SMGerr_SI[:])) # MBB and power
 print ComposTheta_best
 
-SMG_SED_fit = b_nuAndpower(freq_Hz[:], *ComposTheta_best)   
-plt.plot(freq_Hz[:]*Hz2GHz, SMG_SED_fit*SI2Jy*Jy2mJy, '-c', 
-    label='MBB and power fit')
+SMG_SED_fit = b_nuAndpower(freq_Hz[:], *ComposTheta_best)
+plt.plot(freq_Hz[:]*Hz2GHz, SMG_SED_fit*SI2Jy*Jy2mJy, '-c',
+         label='MBB and power fit')
 
-fitParams, fitCovariances  =  curve_fit(B_nu, freq_Hz[-4:], SMGflux_SI[-4:], [1.0e12, 1])       # simple MBB fit using known params
+fitParams, fitCovariances = \
+    curve_fit(B_nu, freq_Hz[-4:], SMGflux_SI[-4:], [1.0e12, 1])       # simple MBB fit using known params
 y_SMG = B_nu(freq_Hz[-4:], *fitParams)
 x_extend = np.linspace(70.e9, 300.e9)
 y_SMG_extend = B_nu(x_extend, *fitParams)       # extrapolate to longer wavelengths
 plt.errorbar(freq_Hz*Hz2GHz, SMG_mJy, SMG_error_mJy, fmt='ko')
-plt.plot(freq_Hz[-4:]*Hz2GHz, y_SMG*SI2Jy*Jy2mJy, 'k-', 
-    linewidth=2, label='simple MBB fit using Haas Params')
+plt.plot(freq_Hz[-4:]*Hz2GHz, y_SMG*SI2Jy*Jy2mJy, 'k-',
+         linewidth=2, label='simple MBB fit using Haas Params')
 
-plt.plot(x_extend*Hz2GHz, y_SMG_extend*SI2Jy*Jy2mJy, 'b--', 
-    linewidth=2, label='extrapolate simple MBB')
+plt.plot(x_extend*Hz2GHz, y_SMG_extend*SI2Jy*Jy2mJy, 'b--',
+         linewidth=2, label='extrapolate simple MBB')
 plt.xlabel('freq [GHz]', fontsize=20,  fontweight='bold')
 plt.title('SMM J0939+8315', fontsize=20,  fontweight='bold')
 plt.ylabel('Flux Density [mJy]', fontsize=20,  fontweight='bold')
 plt.yscale('log')
 plt.xscale('log')
-#plt.legend(loc='best')
+# plt.legend(loc='best')
 plt.grid()
 plt.show()
 
 # Question is why chi2 not similar to mbb-emcee?
-
