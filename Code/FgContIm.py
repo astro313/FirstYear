d04c62510b0a602b4d5fcc8067f3d2b7de7c19d4
Daisy Leung <leungtszkuk@gmail.com>
2015-04-18 01:14:42 -0400

    Got the general plotting going for 3 panels, needs to fix axes locationg, add CARMA beam to 1st panel. Thinking of separating fg and bg plots and possibly combine plots in latex rather than using subplot.

--- Code/FgContIm.py
+++ Code/FgContIm.py
@@ -1,9 +1,10 @@
 from astropy import log
 log.setLevel('ERROR')
 import os, glob
-import aplpy
+import aplpy       # http://aplpy.readthedocs.org/en/stable/howto_subplot.html
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
 
 path = '../FinalData/'
 Plotpath = '../Figure'
@@ -12,8 +13,11 @@ for k in label.iterkeys():
     files = glob.glob(path+'*'+k+'*.fits')
     label[k] = files
 
+# set this ratio for 3 panels (~ square each)
 fig = plt.figure(figsize=(9, 3))
 
+
+# Change contours of yellow VLA 9GHz, take out CARMA zoomed out panel, zoom out VLA panel to inlcude noise.
 
 def get_RA_dec(f):
     """
@@ -30,13 +34,26 @@ def get_RA_dec(f):
 
 def sigma_contour_array(sigma):
     """
-    return list of sigma multiples, -3,-2,2,... 11
+    return list of sigma multiples, starting 4, 8, 16, 32..
+     -- > change to higher sigma, and multiply by ~ 2 for VLA 9 GHz
+    """
+    arr = range(11)
+    arr.remove(0)
+    arr.remove(1)
+    arr = [2**i * sigma for i in arr]
+    arr.append(list(np.array(arr) * -1)[0])
+    print arr
+    return arr
+
+def sigma_contour_CARMA(sigma):
+    """
+    return list of sigma multiples,
     """
     arr = range(-3, 11)
     arr.remove(0)
     arr.remove(1)
     arr.remove(-1)
-    arr = [i * sigma for i in arr]
+    arr = [i * np.sqrt(2) * sigma for i in arr]
     return arr
 
 
@@ -54,41 +71,65 @@ def standard_plot_setup(sp, size):
     sp.ticks.set_linewidth(2)
     sp.ticks.set_length(10)
     sp.ticks.set_minor_frequency(4)
-    # sp.ticks.set_xspacing(45*15/3600.)
+    # sp.ticks.set_xspacing(45*15/3600.)        # deg
 
     sp.axis_labels.set_font(size='medium', weight='bold')      # (size='12')
     sp.axis_labels.set_xtext('Right Ascension (J2000)')
     sp.axis_labels.set_ytext('Declination (J2000)')
     sp.axis_labels.set_xpad(5)
     sp.axis_labels.set_ypad(-15)
+    sp.axis_labels.set_xposition('bottom')
 
 
-def inset(sp):
+def inset_frmt(sp):
     """
-    Hiding the ticks, tick labels (numbers) and axis labels (e.g. RA & Dec.)
+    Hide the ticks, tick labels and axis labels (e.g. RA & Dec.)
     """
     sp.ticks.hide()
     sp.tick_labels.hide()
     sp.axis_labels.hide()
 
 
-def setup_beam(sp, loc='bottom left', major=None, minor=None, angle=None, c='black', hatch=None, a=0.8, lw=3):
+def setup_beam(sp, loc='bottom left', c='black', hatch=None, a=0.8, lw=3):
     """
+    Auto fetch beam info from header
+    Inputs:
+    --------
     loc: str
-    major: deg
-    minor: deg
     hatch: str
         one of /,|,-,+,x,o,O,.,*
     """
     sp.show_beam(corner=loc, color=c, fill=True, edgecolor='grey', facecolor='black', linestyle='solid', linewidth=lw, frame=True, alpha=a)
-    if major != None:
-        sp.beam.set_major(major)
-    if minor != None:
-        sp.beam.set_minor(minor)
-    if angle != None:
-        sp.beam.set_angle(angle)
     if hatch != None:
         sp.beam.set_hatch(hatch)
+
+
+# def setup_beam2(sp, loc='top right', major, minor, angle, c='black', hatch=None, a=0.8, lw=3):
+#     """
+#     Manual input beam inf
+
+#     major: float
+#         deg
+#     minor: float
+#         deg
+#     PA: float
+#         deg
+#     """
+
+#     sp.add_beam(0,0,0)
+#     sp.beam.set_frame(True)
+#     sp.beam.show(major, major, angle)
+#     sp.beam.set(corner=loc, color=c, fill=True, edgecolor='grey', facecolor='black', linestyle='solid', linewidth=lw, frame=True, alpha=a)
+#     sp.beam.set_major(major)  # degrees
+#     sp.beam.set_minor(major)  # degrees
+#     sp.beam.set_angle(angle)  # degrees
+#     sp.beam.set_corner(loc)
+#     sp.beam.set_alpha(a)
+#     sp.beam.set_color('white')
+#     sp.beam.set_edgecolor('white')
+#     sp.beam.set_facecolor('gray')
+#     sp.beam.set_linestyle('solid')
+#     sp.beam.set_linewidth(1)
 
 
 def markers_cross(sp, ra, dec, layer=None, ec='yellow', fc='none', mk='+', s=500, a=1.0, lw=2):
@@ -136,16 +177,16 @@ def setup_scalebar(sp, lg, label, unit=None, loc='bottom right', a=0.9, c='white
     """
     sp.show_scalebar(lg, color=c, corner=loc, alpha=a, linestyle='solid', linewidth=lw)
     sp.scalebar.set_font(size='large', weight='bold')
-    if unit == None:
-        unit = r'$kpc / {\prime\prime}$'
-    sp.scalebar.set_label(label + unit)
+    # if unit == None:
+    #     unit = r'$kpc / {\prime\prime}$'
+    sp.scalebar.set_label(label)
 
 
 ########################################
 # user define area
 ########################################
 sigma_9ghz = 6.41094e-05
-sigma_cont = 0.38466e-3
+sigma_cont = 0.829972e-3       # 0.38466e-3    # 0.829972e-3
 sigma_line = 1.57804
 
 vla_min = -0.000270099
@@ -157,7 +198,7 @@ min_line = -0.2700985e-3
 
 VLA_ra = 1.448437499980E+02
 VLA_dec = 8.325666666846E+01
-sizep = 0.0019
+sizep = 0.0025
 sizePcont_noise = 0.006
 sizePLine = 0.0057
 ra_center = 144.84933
@@ -171,9 +212,9 @@ scale_SMG = 8.252       # kpc/"
 
 # # positions for crosses, centering on radio core
 ra_cross, dec_cross = ra_center, dec_center
-row_a = 0.
+row_a = 0.05
 width = 0.32
-x0 = 0.01
+x0 = 0.05
 dy = 1.0
 
 
@@ -182,7 +223,7 @@ dy = 1.0
 ########################################
 fvla = aplpy.FITSFigure(label['VLA'][0], \
         figure=fig, subplot=[x0,row_a,width,dy])
-fvla.show_grayscale(stretch='log', vmin=vla_min, vmax=vla_max, vmid=-0.001)
+# fvla.show_grayscale(stretch='log', vmin=vla_min, vmax=vla_max, vmid=-0.001)
 
 fcont = aplpy.FITSFigure(label['cont'][0], \
         figure=fig, subplot=[x0+width,row_a,width,dy])
@@ -192,12 +233,24 @@ flin = aplpy.FITSFigure(label['lin'][0], \
         figure=fig, subplot=[x0+width*2,row_a,width,dy])
 flin.show_colorscale(cmap=mpl.cm.jet)#, vmin=min_line, vmax=max_line, vmid=min_line-0.01, stretch='log')
 
+########################################
+# Contours
+########################################
+fvla.show_contour(label['VLA'][0], colors="lime", levels=sigma_contour_array(sigma_9ghz), linewidths=2, layer='fg')
+fvla.show_contour(label['cont'][0], colors='red', levels=sigma_contour_CARMA(sigma_cont), linewidths=2, layer='fg_cont')
+fcont.show_contour(label['cont'][0], colors='white', levels=sigma_contour_CARMA(sigma_cont), linewidths=1.5, layer='fg')
+flin.show_contour(label['lin'][0], colors="white", levels=sigma_contour_CARMA(sigma_line), linewidths=2, layer='mol')
+
 
 ########################################
 # beam
 ########################################
 setup_beam(fvla)
-setup_beam(fvla, )
+bmaj = fcont._header['BMAJ']
+bmin = fcont._header['BMIN']
+bpa = fcont._header['BPA']
+#setup_beam(fvla, loc='top right', major=bmaj, minor=bmin, angle=bpa)
+
 setup_beam(fcont)
 setup_beam(flin)
 
@@ -206,8 +259,11 @@ setup_beam(flin)
 # scale bar
 ########################################
 lg = 1./3600
+
+# --> change to ~ 20kpc
 setup_scalebar(fvla, lg, str(scale_radio))
-setup_scalebar(fcont, lg, str(scale_SMG))
+setup_scalebar(fcont, lg, str(scale_radio))
+setup_scalebar(flin, lg, str(scale_SMG))
 
 
 ########################################
@@ -217,9 +273,9 @@ standard_plot_setup(fvla, sizep)
 standard_plot_setup(fcont, sizePcont_noise)
 standard_plot_setup(flin, sizePLine)
 # fcont.tick_labels.hide()
-# fcont.axis_labels.hide()
+fcont.axis_labels.hide()
 # flin.tick_labels.hide()
-# flin.axis_labels.hide()
+flin.axis_labels.hide()
 
 
 ########################################
@@ -246,24 +302,21 @@ put_label(fvla, 0.80, 0.85, '(a)', 'ref', c=labc, s=labsize)
 put_label(fcont, 0.80, 0.85, '(b)', 'ref', c=labc, s=labsize)
 put_label(flin, 0.80, 0.85, '(c)', 'ref', c=labc, s=labsize)
 
-
 ########################################
-# Contours
+# Colorbar
 ########################################
-fvla.show_contour(label['VLA'][0], colors="yellow", levels=sigma_contour_array(sigma_9ghz), linewidths=2, layer='fg')
-fvla.show_contour(label['cont'][0], colors='red', levels=sigma_contour_array(sigma_cont), linewidths=2, layer='fg')
-fcont.show_contour(label['cont'][0], colors='white', levels=sigma_contour_array(sigma_cont), linewidths=1.5, layer='fg')
-flin.show_contour(label['lin'][0], colors="white", levels=sigma_contour_array(sigma_line), linewidths=2, layer='mol')
-
-
+axisflin = fig.add_axes([0.92,0.19,0.02,0.68])
+normflin = mpl.colors.Normalize(vmin=min_line, vmax=max_line)
+cbflin = mpl.colorbar.ColorbarBase(axisflin, cmap=mpl.cm.jet, norm=normflin, orientation='vertical')
+cbflin.set_label('mJy')
 
 fig.canvas.draw()
 plt.show()
 
 
-if True:
-    os.system('rm -rf blah.png')
-    fig.savefig("ImagePanels.eps", dpi=600)
-    fig.savefig("ImagePanels.png", dpi=300)
+# if save==True:
+#     os.system('rm -rf ImagePanels.png ImagePanels.eps')
+#     fig.savefig("ImagePanels.eps", dpi=600)
+#     fig.savefig("ImagePanels.png", dpi=300)
 
 import sys; sys.exit()
