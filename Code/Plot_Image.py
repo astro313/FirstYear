4e540535773de4a246c65a3ac979bcfa3f7263c0
Daisy Leung <leungtszkuk@gmail.com>
2015-07-18 23:02:07 -0400

    Using the set of mom0, cont from pretty; sigma, max, min changes, mom0 using 72-90 to get 7sigma, also mom0 from 3sigma

--- Code/Plot_Image.py
+++ Code/Plot_Image.py
@@ -21,41 +21,41 @@ fig_line = plt.figure(2, figsize=(12, 5))
 ########################################
 # user define area
 ########################################
-sigma_9ghz = 6.41094e-05
-sigma_cont = 5.0e-4
-sigma_line = 1.663      # chan 65-85 not pretty mom0    # 0.984      # chan72,90 (pretty) mom0 without options=double,mosaic
-sigma_SMA = 0.000836951
+sigma_9ghz          = 6.41094e-05
+sigma_cont          = 5.1e-4
+sigma_line          = 1.0 # 1.35   # chan 65,85 (pretty)     # 1.0      # chan72,90 (pretty) mom0 without options=double,mosaic
+sigma_SMA           = 0.000836951
 
-vla_min = -0.000270099
-vla_max = 0.00960869
-max_cont = 5.347e-3
-min_cont = -1.94e-3
-max_line = 8.93562
-min_line = -8.836704
-SMA_min = -0.00210908
-SMA_max = 0.00634805
+vla_min             = -0.000270099
+vla_max             = 0.00960869
+max_cont            = 4.796309E-03
+min_cont            = -2.038660E-03
+max_line            = 8.93562
+min_line            = -8.836704
+SMA_min             = -0.00210908
+SMA_max             = 0.00634805
 
-VLA_ra = 1.448437499980E+02
-VLA_dec = 8.325666666846E+01
-sizep = 0.0025
-sizePcont_noise = 0.012
-sizePLine = 0.012
-ra_center = 144.84933
-dec_center = 83.257175
+VLA_ra              = 1.448437499980E+02
+VLA_dec             = 8.325666666846E+01
+sizep               = 0.0025
+sizePcont_noise     = 0.012
+sizePLine           = 0.012
+ra_center           = 144.84933
+dec_center          = 83.257175
 
 # WMAP9, flat Universe
-z_radio = 0.685
-scale_radio = 7.169     # kpc/"
-z_SMG = 2.221
-scale_SMG = 8.406       # kpc/"
+z_radio             = 0.685
+scale_radio         = 7.169     # kpc/"
+z_SMG               = 2.221
+scale_SMG           = 8.406       # kpc/"
 
 # # positions for crosses, centering on radio core
 ra_cross, dec_cross = ra_center, dec_center
-row_a = 0.10
-width = 0.35
-x_gap = 0.05
-x0 = 0.10
-dy = 0.90
+row_a               = 0.10
+width               = 0.35
+x_gap               = 0.05
+x0                  = 0.10
+dy                  = 0.90
 
 
 ########################################
@@ -64,7 +64,7 @@ dy = 0.90
 
 fcont = aplpy.FITSFigure(label['cont'][0], \
         figure=fig, subplot=[x0,row_a,width,dy])
-fcont.show_colorscale(cmap=mpl.cm.jet, stretch='log', vmin=min_cont, vmax=max_cont, vmid=-0.5)
+fcont.show_colorscale(cmap=mpl.cm.jet, stretch='log', vmin=min_cont, vmax=max_cont, vmid=-0.8)
 
 fvla = aplpy.FITSFigure(label['VLA'][0], \
         figure=fig, subplot=[x0+width+2*x_gap, row_a, width, dy])
@@ -83,9 +83,9 @@ fSMA.show_grayscale(stretch='log', vmin=SMA_min, vmax=SMA_max, vmid=-0.05)
 fcont.show_contour(label['cont'][0], colors='black', levels=sigma_contour_CARMA(sigma_cont), linewidths=1.5, layer='fg')
 fvla.show_contour(label['VLA'][0], colors="lime", levels=sigma_contour_array(sigma_9ghz), linewidths=2, layer='fg')
 fvla.show_contour(label['cont'][0], colors='red', levels=sigma_contour_CARMA(sigma_cont), linewidths=2, layer='fg_cont')
-flin.show_contour(label['lin'][0], colors="white", levels=sigma_contour_lowSNR(sigma_line), linewidths=2, layer='mol')
+flin.show_contour(label['lin'][0], colors="white", levels=sigma_contour_CARMA(sigma_line), linewidths=2, layer='mol')
 fSMA.show_contour(label['SMA'][0], colors="lime", levels=sigma_contour_CARMA(sigma_SMA), linewidths=1.8, layer='bf_cont')
-fSMA.show_contour(label['lin'][0], colors="red", levels=sigma_contour_lowSNR(sigma_line), linewidths=2, layer='mol')
+fSMA.show_contour(label['lin'][0], colors="red", levels=sigma_contour_CARMA(sigma_line), linewidths=2, layer='mol')
 
 ########################################
 # beam
@@ -141,12 +141,12 @@ markers_cross(fSMA, ra_cross, dec_cross, layer='marker_set_1')
 ########################################
 put_label(fvla, 0.15, 0.95, 'VLA 9GHz', 'titleBand')
 put_label(fvla, 0.17, 0.9, '3C220.3', 'titleObj')
-put_label(fcont, 0.31, 0.95, 'CARMA 104GHz', 'titleBand')
-put_label(fcont, 0.2625, 0.9, '3C220.3', 'titleObj')
-put_label(flin, 0.31, 0.90, 'CARMA CO(3-2)', 'titleBand', c='black')
-put_label(flin, 0.31, 0.85, 'SMM J0939+8315', 'titleObj', c='black')
-put_label(fSMA, 0.40, 0.90, 'SMA 1 mm, CARMA CO(3-2)', 'titleBand')
-put_label(fSMA, 0.2625, 0.85, 'SMM J0939+8315', 'titleObj')
+put_label(fcont, 0.31, 0.95, 'CARMA 104GHz', 'titleBand', c='black')
+put_label(fcont, 0.2625, 0.9, '3C220.3', 'titleObj', c='black')
+put_label(flin, 0.31, 0.93, 'CARMA CO(3-2)', 'titleBand', c='black')
+put_label(flin, 0.31, 0.87, 'SMM J0939+8315', 'titleObj', c='black')
+put_label(fSMA, 0.40, 0.95, 'SMA 1 mm, CARMA CO(3-2)', 'titleBand')
+put_label(fSMA, 0.2625, 0.9, 'SMM J0939+8315', 'titleObj')
 
 labsize = 'xx-large'
 labc = 'white'
@@ -178,11 +178,11 @@ if __name__ == '__main__':
     if saveFig == True:
         outfile = 'ContPanel'
         outfile_line = 'LinePanel'
-        os.system('rm -rf ' + outfile + '.png' + ' ' + outfile + '.eps')
-        os.system('rm -rf ' + outfile_line + '.png' + ' ' + outfile_line + '.eps')
-        fig_line.savefig('../Figure/'+outfile_line+'.eps', dpi=600)
+        os.system('rm -rf ' + outfile + '.png' + ' ' + outfile + '.pdf')
+        os.system('rm -rf ' + outfile_line + '.png' + ' ' + outfile_line + '.pdf')
+        fig_line.savefig('../Figure/'+outfile_line+'.pdf', dpi=600)
         fig_line.savefig('../Figure/'+outfile_line+'.png', dpi=300)
-        fig.savefig('../Figure/'+outfile+'.eps', dpi=600)
+        fig.savefig('../Figure/'+outfile+'.pdf', dpi=600)
         fig.savefig('../Figure/'+outfile+'.png', dpi=300)
     else:
         plt.show()
