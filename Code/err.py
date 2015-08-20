305e67d9de0846ff0902db08359100f43252c1d4
Daisy Leung <leungtszkuk@gmail.com>
2015-08-20 11:30:53 -0400

    Edit since DR comments on July31 version

--- Code/err.py
+++ Code/err.py
@@ -21,13 +21,16 @@ Mgas = 2.326e10    # with lensing correction
 
 #####################
 #####################
-sig_LprimeCO = (3.25e7 / freq_CO32**2 * dl**2 / (1+z)) * ((del_I/mu)**2 + (del_mu/mu**2*I_line)**2)**(.5)
-del_LprimeCO = 3.25e7 / freq_CO32**2 * dl**2 / (1+z) * ((del_I/mu) + (del_mu/mu**2*I_line))
+sig_LprimeCO = (3.25e7 / freq_CO32 ** 2 * dl ** 2 / (1 + z)) * \
+    ((del_I / mu) ** 2 + (del_mu / mu ** 2 * I_line) ** 2) ** (.5)
+del_LprimeCO = 3.25e7 / freq_CO32 ** 2 * dl ** 2 / \
+    (1 + z) * ((del_I / mu) + (del_mu / mu ** 2 * I_line))
 
-sig_LprimeCO_noMu = (3.25e7 / freq_CO32**2 * dl**2 / (1+z)) * (del_I/mu)
-del_LprimeCO_noMu = 3.25e7 / freq_CO32**2 * dl**2 / (1+z) * (del_I/mu)
+sig_LprimeCO_noMu = (3.25e7 / freq_CO32 ** 2 * dl ** 2 / (1 + z)) * (del_I)
+del_LprimeCO_noMu = 3.25e7 / freq_CO32 ** 2 * dl ** 2 / (1 + z) * (del_I)
 
-LprimCO_noMu = 3.25e7/freq_CO32**2 * dl**2 / (1+z) * I_line
+LprimCO_noMu = 3.25e7 / freq_CO32 ** 2 * dl ** 2 / (1 + z) * I_line
+#print str(LprimCO_noMu/1.e10) + '+/-' + str(sig_LprimeCO_noMu / 1.e10)
 
 sig_Mgas = alpha * sig_LprimeCO
 del_Mgas = alpha * del_LprimeCO
@@ -37,59 +40,71 @@ sig_Mgas_noMu = alpha * sig_LprimeCO_noMu
 del_Mgas_noMu = alpha * del_LprimeCO_noMu
 
 # With lensing correction, M_dust
-sig_Mdust = ((M_dust/mu**2 * del_mu)**2 + (sig_Mdust_noMu/mu)**2)**(.5)
-del_Mdust = M_dust/mu**2 * del_mu + sig_Mdust_noMu/mu
+sig_Mdust = (
+    (M_dust / mu ** 2 * del_mu) ** 2 + (sig_Mdust_noMu / mu) ** 2) ** (.5)
+del_Mdust = M_dust / mu ** 2 * del_mu + sig_Mdust_noMu / mu
 
 # with lensing correciton, LIR
-sig_LIR = ((del_IR_noMu/mu)**2 + (LIR/mu**2 * del_mu)**2)**(0.5)
-del_LIR = del_IR_noMu/mu + LIR/mu**2 * del_mu
+sig_LIR = ((del_IR_noMu / mu) ** 2 + (LIR / mu ** 2 * del_mu) ** 2) ** (0.5)
+del_LIR = del_IR_noMu / mu + LIR / mu ** 2 * del_mu
 
-sig_Mdyn = 2.8e5 * ((2. * vel * del_vel * rkpc)**2 + (vel**2 * del_kpc)**2)**(.5)
-del_Mdyn = 2.8e5 * (2. * vel * del_vel * rkpc + vel**2 * del_kpc)
+sig_Mdyn = 2.8e5 * \
+    ((2. * vel * del_vel * rkpc) ** 2 + (vel ** 2 * del_kpc) ** 2) ** (.5)
+del_Mdyn = 2.8e5 * (2. * vel * del_vel * rkpc + vel ** 2 * del_kpc)
 
-sig_SFR = 1.e-10 * ((LIR/mu**2*del_mu)**2 + (del_IR_noMu/mu)**2)**(.5)
-del_SFR = 1.e-10 * (LIR/mu**2*del_mu + del_IR_noMu/mu)
+sig_SFR = 1.e-10 * \
+    ((LIR / mu ** 2 * del_mu) ** 2 + (del_IR_noMu / mu) ** 2) ** (.5)
+del_SFR = 1.e-10 * (LIR / mu ** 2 * del_mu + del_IR_noMu / mu)
 
-sig_SFR_FIR = 1.e-10 * ((LFIR/mu**2*del_mu)**2 + (del_FIR_noMu/mu)**2)**(.5)
-del_SFR_FIR = 1.e-10 * (LFIR/mu**2*del_mu + del_FIR_noMu/mu)
+sig_SFR_FIR = 1.e-10 * \
+    ((LFIR / mu ** 2 * del_mu) ** 2 + (del_FIR_noMu / mu) ** 2) ** (.5)
+del_SFR_FIR = 1.e-10 * (LFIR / mu ** 2 * del_mu + del_FIR_noMu / mu)
 
 SFR_noMu = 1.e-10 * LIR
-sig_SFR_noMu = 1.e-10 * (del_IR_noMu/mu)
-del_SFR_noMu = 1.e-10 * (del_IR_noMu/mu)
+sig_SFR_noMu = 1.e-10 * (del_IR_noMu / mu)
+del_SFR_noMu = 1.e-10 * (del_IR_noMu / mu)
 
-sig_tauDepl = ((sig_Mgas_noMu/SFR_noMu)**2 + (Mgas_noMu/SFR_noMu**2*sig_SFR_noMu)**2)**(.5)
-del_tauDepl = (del_Mgas_noMu/SFR_noMu) + (Mgas_noMu/SFR_noMu**2*del_SFR_noMu)
+sig_tauDepl = ((sig_Mgas_noMu / SFR_noMu) ** 2 +
+               (Mgas_noMu / SFR_noMu ** 2 * sig_SFR_noMu) ** 2) ** (.5)
+del_tauDepl = (del_Mgas_noMu / SFR_noMu) + \
+    (Mgas_noMu / SFR_noMu ** 2 * del_SFR_noMu)
 
-sig_SFE = ((del_IR_noMu/LprimCO_noMu)**2 + (sig_LprimeCO_noMu/LprimCO_noMu**2*LIR)**2)**(.5)
-del_SFE = del_IR_noMu/LprimCO_noMu + del_LprimeCO_noMu/LprimCO_noMu**2 * LIR
+sig_SFE = ((del_IR_noMu / LprimCO_noMu) ** 2 +
+           (sig_LprimeCO_noMu / LprimCO_noMu ** 2 * LIR) ** 2) ** (.5)
+# print str(LprimCO_noMu/1.e10)+'+/-'+str(sig_LprimeCO_noMu/1.e10)
 
-sig_fgasdust = ((Mgas_noMu/M_dust**2*sig_Mdust_noMu)**2 + (sig_Mgas_noMu/M_dust)**2)**(.5)
-del_fgasdust = Mgas_noMu/M_dust**2*sig_Mdust_noMu + del_Mgas_noMu/M_dust
+del_SFE = del_IR_noMu / LprimCO_noMu + \
+    del_LprimeCO_noMu / LprimCO_noMu ** 2 * LIR
 
-sig_fgasdyn = ((sig_Mgas/M_dyn)**2 + (Mgas/M_dyn**2*sig_Mdyn)**2)**(.5)
-del_fgasdyn = del_Mgas/M_dyn + Mgas/M_dyn**2*del_Mdyn
+sig_fgasdust = ((Mgas_noMu / M_dust ** 2 * sig_Mdust_noMu)
+                ** 2 + (sig_Mgas_noMu / M_dust) ** 2) ** (.5)
+del_fgasdust = Mgas_noMu / M_dust ** 2 * sig_Mdust_noMu + del_Mgas_noMu / M_dust
 
-print "delta Lprime CO: ", sig_LprimeCO/1.e10
-print "delta Mgas: ", sig_Mgas/1.e10
-print "delta_Mdyn: ", sig_Mdyn/1.e10
-print "delta Mdust with lensing corretion: ", sig_Mdust/1.e8
-print "Delta LIR with lensing corretion: ", sig_LIR/1.e12
+sig_fgasdyn = (
+    (sig_Mgas / M_dyn) ** 2 + (Mgas / M_dyn ** 2 * sig_Mdyn) ** 2) ** (.5)
+del_fgasdyn = del_Mgas / M_dyn + Mgas / M_dyn ** 2 * del_Mdyn
+
+print "delta Lprime CO: ", sig_LprimeCO / 1.e10
+print "delta Mgas: ", sig_Mgas / 1.e10
+print "delta_Mdyn: ", sig_Mdyn / 1.e10
+print "delta Mdust with lensing corretion: ", sig_Mdust / 1.e8
+print "Delta LIR with lensing corretion: ", sig_LIR / 1.e12
 print "delta SFR: ", sig_SFR
 print "delta SFR_FIR: ", sig_SFR_FIR
-print "delta_Tau: ", sig_tauDepl/1.e6
+print "delta_Tau: ", sig_tauDepl / 1.e6
 print "delta_SFE: ", sig_SFE
 print "delta f gas dust: ", sig_fgasdust
 print "delta f gas dyn: ", sig_fgasdyn
 
 
-print "delta Lprime CO: ", del_LprimeCO/1.e10
-print "delta Mgas: ", del_Mgas/1.e10
-print "delta_Mdyn: ", del_Mdyn/1.e10
-print "delta Mdust with lensing corretion: ", del_Mdust/1.e8
-print "Delta LIR with lensing corretion: ", del_LIR/1.e12
+print "delta Lprime CO: ", del_LprimeCO / 1.e10
+print "delta Mgas: ", del_Mgas / 1.e10
+print "delta_Mdyn: ", del_Mdyn / 1.e10
+print "delta Mdust with lensing corretion: ", del_Mdust / 1.e8
+print "Delta LIR with lensing corretion: ", del_LIR / 1.e12
 print "delta SFR: ", del_SFR
 print "delta SFR_FIR: ", del_SFR_FIR
-print "delta_Tau: ", del_tauDepl/1.e6
+print "delta_Tau: ", del_tauDepl / 1.e6
 print "delta_SFE: ", del_SFE
 print "delta f gas dust: ", del_fgasdust
 print "delta f gas dyn: ", del_fgasdyn
